37f7a936f462e2940520bff3712506529c9aa9a5
DesnLee <jiakun.ui@gmail.com>
2023-10-21 23:34:07 +0800

    refactor: refactor time type

--- internal/model/item.go
+++ internal/model/item.go
@@ -1,19 +1,20 @@
 package model
 
 import (
-	"time"
+	"github.com/jackc/pgx/v5/pgtype"
 )
 
 type CreateItemRequestBody struct {
-	Amount     int64     `json:"amount" binding:"required"`
-	Kind       string    `json:"kind" binding:"required"`
-	HappenedAt time.Time `json:"happened_at" binding:"required"`
-	TagIDs     []int64   `json:"tag_ids" binding:"required"`
+	Amount     int64              `json:"amount" binding:"required"`
+	Kind       string             `json:"kind" binding:"required"`
+	HappenedAt pgtype.Timestamptz `json:"happened_at" binding:"required"`
+	TagIDs     []int64            `json:"tag_ids" binding:"required"`
 }
 
 type CreateItemResponseBody struct {
-	ID         int64     `json:"id"`
-	Amount     int64     `json:"amount"`
-	Kind       string    `json:"kind"`
-	HappenedAt time.Time `json:"happened_at"`
+	ID         int64              `json:"id"`
+	Amount     int64              `json:"amount"`
+	Kind       string             `json:"kind"`
+	HappenedAt pgtype.Timestamptz `json:"happened_at"`
+	TagIDs     []int64            `json:"tag_ids"`
 }
