df7bf1c5c0d2f29a73cca4b21054a59d77c6b72c
DesnLee <jiakun.ui@gmail.com>
2023-10-21 14:49:07 +0800

    doc: add ping doc

--- internal/controller/ping_controller.go
+++ internal/controller/ping_controller.go
@@ -7,6 +7,13 @@ import (
 	"ppapi.desnlee.com/internal/model"
 )
 
+// PingHandler godoc
+//
+//	@Summary		接口健康检查
+//	@Description	检查接口是否正常
+//	@Tags			基础
+//	@Success		200
+//	@Router			/ping [get]
 func PingHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, model.MsgResponse{
 		Msg: "pong",
