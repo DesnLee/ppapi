84d2d3b86fe4255d4d2cba1162792646cffa84b9
DesnLee <jiakun.ui@gmail.com>
2023-11-11 16:06:31 +0800

    fix: fix page bug

--- internal/controller/controller_helper/item_helper.go
+++ internal/controller/controller_helper/item_helper.go
@@ -203,15 +203,17 @@ func generateQueryItemsCondition(uid pgtype.UUID, b model.GetItemsRequestBody) (
 		UserID: uid,
 	}
 	// 初始化未传字段
-	if b.Page == 0 {
-		condition.Offset = 0
-	} else {
-		condition.Offset = int32((b.Page - 1) * b.Size)
-	}
 	if b.Size == 0 {
 		condition.Limit = 10
+		b.Size = 10
 	} else {
 		condition.Limit = int32(b.Size)
+	}
+	if b.Page == 0 {
+		condition.Offset = 0
+		b.Page = 1
+	} else {
+		condition.Offset = int32((b.Page - 1) * b.Size)
 	}
 
 	after, before := generateDateRange(b.HappenedAfter, b.HappenedBefore)
