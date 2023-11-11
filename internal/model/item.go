a4486256dc3f3837aff0517254519d658d0c3293
DesnLee <jiakun.ui@gmail.com>
2023-11-11 15:57:12 +0800

    fix: fix doc example

--- internal/model/item.go
+++ internal/model/item.go
@@ -7,7 +7,7 @@ import (
 type CreateItemRequestBody struct {
 	Amount     int64         `json:"amount" binding:"required" example:"100"` // 金额单位为（分）
 	Kind       sqlcExec.Kind `json:"kind" binding:"required" swaggertype:"string" enums:"income,expenses" example:"expenses"`
-	HappenedAt string        `json:"happened_at" binding:"required" example:"2023-01-01"`
+	HappenedAt string        `json:"happened_at" binding:"required" example:"2023-01-01T00:00:00+08:00"`
 	TagIDs     []int64       `json:"tag_ids" binding:"required" example:"1"`
 }
 
@@ -15,7 +15,7 @@ type CreateItemResponseData struct {
 	ID         int64         `json:"id" example:"1"`
 	Amount     int64         `json:"amount" example:"100"`
 	Kind       sqlcExec.Kind `json:"kind" swaggertype:"string" example:"expenses" enums:"income,expenses"`
-	HappenedAt string        `json:"happened_at"  example:"2023-01-01"`
+	HappenedAt string        `json:"happened_at"  example:"2023-01-01T00:00:00+08:00"`
 	TagIDs     []int64       `json:"tag_ids" example:"1"`
 }
 type CreateItemResponseSuccessBody = ResourceResponse[CreateItemResponseData]
@@ -23,8 +23,8 @@ type CreateItemResponseSuccessBody = ResourceResponse[CreateItemResponseData]
 type GetItemsRequestBody struct {
 	Page           int64  `form:"page" example:"1"`
 	Size           int64  `form:"size" example:"10"`
-	HappenedBefore string `form:"happened_before" example:"2023-01-01"`
-	HappenedAfter  string `form:"happened_after" example:"2023-01-01"`
+	HappenedBefore string `form:"happened_before" example:"2023-01-01T00:00:00+08:00"`
+	HappenedAfter  string `form:"happened_after" example:"2023-01-01T00:00:00+08:00"`
 }
 type GetItemsResponseData struct {
 	CreateItemResponseData
@@ -33,8 +33,8 @@ type GetItemsResponseData struct {
 type GetItemsResponseSuccessBody = ResourcesResponse[GetItemsResponseData]
 
 type GetBalanceRequestBody struct {
-	HappenedBefore string `form:"happened_before" example:"2023-01-01"`
-	HappenedAfter  string `form:"happened_after" example:"2023-01-01"`
+	HappenedBefore string `form:"happened_before" example:"2023-01-01T00:00:00+08:00"`
+	HappenedAfter  string `form:"happened_after" example:"2023-01-01T00:00:00+08:00"`
 }
 type GetBalanceResponseData struct {
 	Income   int64 `json:"income" example:"100"`
