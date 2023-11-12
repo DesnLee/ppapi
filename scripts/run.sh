fd1bad39aa0d6227842cb68b63da518b157b088a
DesnLee <jiakun.ui@gmail.com>
2023-11-12 10:38:23 +0800

    fix: add parseDependency flag for swag init

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-sqlc generate && swag fmt && swag init && go build -o ppapi && ./ppapi server
+sqlc generate && swag fmt && swag init --parseDependency && go build -o ppapi && ./ppapi server
